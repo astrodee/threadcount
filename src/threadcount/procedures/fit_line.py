ad44c3f627d50eaf31dde8e8c4bad365c7f24b17
Deanne Fisher <dfisher@swin.edu.au>
2024-02-15 14:40:29 +1100

    Update fit_line.py
    fixed chop_bandwidth spelling error

--- src/threadcount/procedures/fit_line.py
+++ src/threadcount/procedures/fit_line.py
@@ -194,7 +194,7 @@ def process_single_spectrum(subcube_av, snr_image, snr_threshold, models, s, idx
         return [None]
 
     if f.success is False:
-        if s.chop_bandwith is False:
+        if s.chop_bandwidth is False:
             return [None]
         # One reason we saw for 1 gaussian fit to fail includes the iron line when
         # fitting 5007. Therefore, if there is a failure to fit 1 gaussian, I will
